b2b29df86e3f7d7c9707ac8ab194377d817fd02e
milangmatt <milangeorgem@gmail.com>
2024-03-26 01:28:01 +0530

    change

--- Sched3.c
+++ Sched3.c
@@ -140,37 +140,73 @@ void sjf() {
 
 // Function for Priority scheduling
 void priority() {
-    int i, j, n, num, idle = 0;
-    float avwt = 0, avtt = 0;
+    int n, i, j, ls, min, fnd, num, idle;
+    float twt = 0.0, ttt = 0.0;
     printf("ENTER THE NUMBER OF PROCESSES : ");
     scanf("%d", &n);
     struct Process p[n];
-    inputProcessInfo(p, n);
-
-    // Sorting based on arrival time
-    for (i = 0; i < n - 1; i++) {
-        for (j = 0; j < n - i - 1; j++) {
-            if (p[j].at > p[j + 1].at) {
-                struct Process temp = p[j];
-                p[j] = p[j + 1];
-                p[j + 1] = temp;
+    struct Done d[2 * n];
+    for (i = 0; i < n; i++) // Input process details
+    {
+        printf("\nENTER DETAILS OF PROCESS %d", i + 1);
+        printf("\nPROCESS NAME : ");
+        scanf(" %s", p[i].name);
+        printf("ARRIVAL TIME : ");
+        scanf("%d", &p[i].at);
+        printf("BURST TIME : ");
+        scanf("%d", &p[i].bt);
+        printf("PRIORITY : ");
+        scanf("%d", &p[i].pr);
+        p[i].status = 0;
+    }
+    for (i = 0, ls = 0, num = 0, idle = 0; ls < n;)
+    {
+        for (j = 0, fnd = 0; j < n; j++)
+        {
+            if (i >= p[j].at && p[j].status == 0)
+            {
+                if (fnd == 0)
+                {
+                    min = j;
+                    fnd = 1;
+                }
+                else if ((p[min].pr > p[j].pr) || (p[min].pr == p[j].pr && p[min].at > p[j].at))
+                {
+                    min = j;
+                }
             }
         }
+        if (idle == 0 && fnd == 0)
+        {
+            strcpy(d[num].name, "Idle");
+            d[num].st = i;
+            i++;
+            idle = 1;
+        }
+        else if (fnd == 1)
+        {
+            if (idle == 1)
+            {
+                d[num].ct = i;
+                num++;
+                idle = 0;
+            }
+            strcpy(d[num].name, p[min].name);
+            p[min].status = 1;
+            d[num].st = i;
+            d[num].ct = i + p[min].bt;
+            i = d[num].ct;
+            p[min].ct = d[num].ct;
+            p[min].tt = p[min].ct - p[min].at;
+            p[min].wt = p[min].tt - p[min].bt;
+            num++;
+            ls++;
+        }
+        else
+        {
+            i++;
+        }
     }
-
-    // Priority Scheduling Algorithm
-    num = 0;
-    struct Done d[2 * n];
-    for (i = 0; i < n; i++) {
-        d[num].st = p[i].at;
-        strcpy(d[num].name, p[i].name);
-        d[num].ct = d[num].st + p[i].bt;
-        p[i].wt = d[num].st - p[i].at;
-        p[i].tt = p[i].wt + p[i].bt;
-        p[i].ct = d[num].ct;
-        num++;
-    }
-
     // Print Gantt chart
     printGanttChart(d, num);
 
